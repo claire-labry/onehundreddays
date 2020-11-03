7f235baf010709a6b3d144401c2b89feda1e52f7
claire-labry <clairestjlabry@gmail.com>
2020-11-03 14:12:50 -0600

    completed day 1 of 100 days; updated the random button

--- 001/script.js
+++ 001/script.js
@@ -58,6 +58,22 @@ function getMealById(mealID) {
     });
 }
 
+
+// Fetch random meal from API
+function getRandomMeal() {
+    // clear meals & heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+
+        addMealToDOM(meal);
+    })
+}
+
 // add meal to DOM
 function addMealToDOM(meal) {
   const ingredients = [];
@@ -92,6 +108,7 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', (e) => {
   const mealInfo = e.path.find((item) => {
