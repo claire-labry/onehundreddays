c6c2f4c49fefb80c08bd1c0bac17a60601fefd61
claire-labry <clairestjlabry@gmail.com>
2020-11-06 10:57:15 -0600

    completed day 3 a little late; added the progress bar functions

--- 003/script.js
+++ 003/script.js
@@ -1,7 +1,7 @@
 const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
-const nexBtn = document.getElementById('next');
+const nextBtn = document.getElementById('next');
 
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
@@ -43,6 +43,48 @@ function pauseSong() {
     audio.pause();
 }
 
+// previous song
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }   
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// next song
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }   
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 // event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -52,4 +94,17 @@ playBtn.addEventListener('click', () => {
     } else {
         playSong()
     }
-})
+});
+
+// change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// song ends
+audio.addEventListener('ended', nextSong);
