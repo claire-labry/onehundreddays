8e22263ac8a360b936dcdbb7a95580c221ceb15e
claire-labry <clairestjlabry@gmail.com>
2020-11-10 19:09:31 -0600

    added the timer function

--- 005/script.js
+++ 005/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -41,6 +41,12 @@ let score = 0;
 // init time
 let time = 10;
 
+// focus on text on start
+text.focus();
+
+// start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // generate random word from array
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
@@ -56,6 +62,29 @@ function addWordToDOM() {
 function updateScore() {
     score++;
     scoreEl.innerHTML = score;
+}
+
+// update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's'
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        // end game
+        gameOver();
+    }
+}
+
+// game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML =  `
+        <h1>Time ran out!</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick='location.reload()'>Try Again?</button>
+    `;
+
+    endgameEl.style.display = 'flex'
 }
 
 addWordToDOM();
