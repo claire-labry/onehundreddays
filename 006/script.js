6b33b4cedce347f980be79a05d8a2e6f9957a756
claire-labry <clairestjlabry@gmail.com>
2020-11-11 12:59:09 -0600

    completing the first part of this project with html and javascript

---
+++ 006/script.js
@@ -0,0 +1,76 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: './img/hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: './img/happy.jpg',
+        text: "I'm Happy"
+    },
+    {
+        image: './img/angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: './img/sad.jpg',
+        text: "I'm Sad"
+    },
+    {
+        image: './img/scared.jpg',
+        text: "I'm Scared"
+    },
+    {
+        image: './img/tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: './img/outside.jpg',
+        text: "I want to go outside"
+    },
+    {
+        image: './img/home.jpg',
+        text: "I want to go home"
+    },
+    {
+        image: './img/school.jpg',
+        text: "I want to go to school"
+    },
+    {
+        image: './img/grandma.jpg',
+        text: "I want to go to grandmas"
+    },
+];
+
+data.forEach(createBox);
+
+// create speach box
+function createBox(item) {
+ const box = document.createElement('div');
+
+ const { image, text } = item;
+
+ box.classList.add('box');
+ box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+ `;
+
+//  @todo - speak event
+
+main.appendChild(box);
+}
