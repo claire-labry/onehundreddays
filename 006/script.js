cd484402f950a6432c0404cc9f08958ec1cd85c2
claire-labry <clairestjlabry@gmail.com>
2020-11-11 13:52:16 -0600

    adding text to speech api from mozilla

--- 006/script.js
+++ 006/script.js
@@ -6,71 +6,101 @@ const toggleBtn = document.getElementById('toggle');
 const closeBtn = document.getElementById('close');
 
 const data = [
-    {
-        image: './img/drink.jpg',
-        text: "I'm Thirsty"
-    },
-    {
-        image: './img/food.jpg',
-        text: "I'm Hungry"
-    },
-    {
-        image: './img/hurt.jpg',
-        text: "I'm Hurt"
-    },
-    {
-        image: './img/happy.jpg',
-        text: "I'm Happy"
-    },
-    {
-        image: './img/angry.jpg',
-        text: "I'm Angry"
-    },
-    {
-        image: './img/sad.jpg',
-        text: "I'm Sad"
-    },
-    {
-        image: './img/scared.jpg',
-        text: "I'm Scared"
-    },
-    {
-        image: './img/tired.jpg',
-        text: "I'm Tired"
-    },
-    {
-        image: './img/outside.jpg',
-        text: "I want to go outside"
-    },
-    {
-        image: './img/home.jpg',
-        text: "I want to go home"
-    },
-    {
-        image: './img/school.jpg',
-        text: "I want to go to school"
-    },
-    {
-        image: './img/grandma.jpg',
-        text: "I want to go to grandmas"
-    },
+  {
+    image: './img/drink.jpg',
+    text: "I'm Thirsty",
+  },
+  {
+    image: './img/food.jpg',
+    text: "I'm Hungry",
+  },
+  {
+    image: './img/hurt.jpg',
+    text: "I'm Hurt",
+  },
+  {
+    image: './img/happy.jpg',
+    text: "I'm Happy",
+  },
+  {
+    image: './img/angry.jpg',
+    text: "I'm Angry",
+  },
+  {
+    image: './img/sad.jpg',
+    text: "I'm Sad",
+  },
+  {
+    image: './img/scared.jpg',
+    text: "I'm Scared",
+  },
+  {
+    image: './img/tired.jpg',
+    text: "I'm Tired",
+  },
+  {
+    image: './img/outside.jpg',
+    text: 'I want to go outside',
+  },
+  {
+    image: './img/home.jpg',
+    text: 'I want to go home',
+  },
+  {
+    image: './img/school.jpg',
+    text: 'I want to go to school',
+  },
+  {
+    image: './img/grandma.jpg',
+    text: 'I want to go to grandmas',
+  },
 ];
 
 data.forEach(createBox);
 
 // create speach box
 function createBox(item) {
- const box = document.createElement('div');
+  const box = document.createElement('div');
 
- const { image, text } = item;
+  const { image, text } = item;
 
- box.classList.add('box');
- box.innerHTML = `
+  box.classList.add('box');
+  box.innerHTML = `
     <img src="${image}" alt="${text}" />
     <p class="info">${text}</p>
  `;
 
-//  @todo - speak event
+  //  @todo - speak event
 
-main.appendChild(box);
+  main.appendChild(box);
 }
+
+// store voices
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voicesSelect.appendChild(option);
+    });
+}
+
+// voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+// toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show'),
+);
+
+// close button
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show'),
+);
+
+getVoices();
