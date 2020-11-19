ab50b741031fd2062c6cb2135d9b714769c45fb1
claire-labry <clairestjlabry@gmail.com>
2020-11-19 17:44:49 -0600

    ran into a cors issue, stopping here

--- 008/script.js
+++ 008/script.js
@@ -7,11 +7,51 @@ const apiURL = 'https://api.lyrics.ovh';
 
 // search by song or artist
 async function searchSongs(term) {
-    const res = await fetch(`${apiURL}/suggest/${term}`);
-    const data = await res.json();
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
 
-    showData(data);
+  showData(data);
 }
+
+// show song and artist in the DOM
+function showData(data) {
+  result.innerHTML = `
+        <ul class="songs">
+            ${data.data
+              .map(
+                (song) => `<li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-song="${song.title}">Get Lyrics</button>
+        </li>`,
+              )
+              .join('')}
+        </ul>
+        `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+            ${
+              data.prev
+                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                : ''
+            }
+            ${
+              data.next
+                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                : ''
+            }`;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// get prev and next songs
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+  
+    showData(data);
+  }
 
 // event listeners
 form.addEventListener('submit', (e) => {
